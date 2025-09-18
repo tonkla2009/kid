fe6eb21d97636b67d56b0ff67c41eca8951c1d68
V <145551274+tonkla2009@users.noreply.github.com>
2025-09-18 15:39:14 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,16 @@
+let Sensor4 = false
+let Sensor3 = false
+let Sensor2 = false
+let Sensor1 = false
 dadabit.dadabit_init()
 wondercam.ChangeFunc(wondercam.Functions.ColorDetect)
-let Sensor1 = dadabit.line_followers(dadabit.LineFollowerSensors.S1, dadabit.LineColor.Black)
-let Sensor2 = dadabit.line_followers(dadabit.LineFollowerSensors.S2, dadabit.LineColor.Black)
-let Sensor3 = dadabit.line_followers(dadabit.LineFollowerSensors.S3, dadabit.LineColor.Black)
-let Sensor4 = dadabit.line_followers(dadabit.LineFollowerSensors.S4, dadabit.LineColor.Black)
 basic.pause(500)
 basic.forever(function () {
     wondercam.UpdateResult()
+    Sensor1 = dadabit.line_followers(dadabit.LineFollowerSensors.S1, dadabit.LineColor.Black)
+    Sensor2 = dadabit.line_followers(dadabit.LineFollowerSensors.S2, dadabit.LineColor.Black)
+    Sensor3 = dadabit.line_followers(dadabit.LineFollowerSensors.S3, dadabit.LineColor.Black)
+    Sensor4 = dadabit.line_followers(dadabit.LineFollowerSensors.S4, dadabit.LineColor.Black)
     if (Sensor2 && Sensor3) {
         dadabit.setLego360Servo(1, dadabit.Oriention.Counterclockwise, 100)
         dadabit.setLego360Servo(2, dadabit.Oriention.Clockwise, 100)
